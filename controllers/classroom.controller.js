11440144d7299518044d1328acdbaa61b25adae5
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-02-24 20:32:21 +0530

    bug fixes

--- controllers/classroom.controller.js
+++ controllers/classroom.controller.js
@@ -19,6 +19,18 @@ const getClassroomById = async (req, res, next) => {
   }
 };
 
+const getJoinedStudents = async (req, res, next) => {
+  const { classroom_id } = req.params;
+  const query = `select students.name as student_name, students.email as student_email from students_classrooms inner join students on students_classrooms.student_id = students.student_id where classroom_id = '${classroom_id}'`;
+  try {
+    const result = await client.query(query);
+    res.json(result.rows);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ message: "Error" });
+  }
+};
+
 const createClassroom = async (req, res, next) => {
   const { name, description, section } = req.body;
   if (req.user.userType !== "teacher") {
@@ -38,7 +50,12 @@ const getCreatedClassrooms = async (req, res, next) => {
   if (req.user.userType !== "teacher") {
     return res.status(400).send({ message: "You cannot get created classrooms" });
   }
-  const query = `select classrooms.*, count(students_classrooms.student_id) as students_count from classrooms inner join students_classrooms on classrooms.classroom_id = students_classrooms.classroom_id where classrooms.teacher_id = '${req.user.userId}' group by classrooms.classroom_id`;
+
+  const query = `select classrooms.* from classrooms inner join teachers on classrooms.teacher_id = teachers.teacher_id where classrooms.teacher_id = '${req.user.userId}'`;
+  // get created classrooms with number of students erolled
+  // const query = `select classrooms.*, count(students_classrooms.student_id) as students_count from classrooms inner join teachers on classrooms.teacher_id = teachers.teacher_id inner join students_classrooms on classrooms.classroom_id = students_classrooms.classroom_id where classrooms.teacher_id = '${req.user.userId}' group by classrooms.classroom_id`;
+  // get created classrooms with number of students enrolled in each classroom group by classroom_id
+  //const query = `select classrooms.*, teachers.name as teacher_name, teachers.email as teacher_email, count(students_classrooms.student_id) as students_count from classrooms inner join teachers on classrooms.teacher_id = teachers.teacher_id inner join students_classrooms on classrooms.classroom_id = students_classrooms.classroom_id where classrooms.teacher_id = '${req.user.userId}' group by classrooms.classroom_id`;
 
   try {
     const result = await client.query(query);
@@ -90,4 +107,4 @@ const getJoinedClassrooms = async (req, res, next) => {
   }
 };
 
-module.exports = { getClassroomById, createClassroom, getCreatedClassrooms, joinClassroom, getJoinedClassrooms };
+module.exports = { getClassroomById, createClassroom, getCreatedClassrooms, joinClassroom, getJoinedClassrooms, getJoinedStudents };
--- routes/classroom.routes.js
+++ routes/classroom.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getClassroomById, getJoinedClassrooms, getCreatedClassrooms, createClassroom, joinClassroom } = require("../controllers/classroom.controller");
+const { getClassroomById, getJoinedClassrooms, getCreatedClassrooms, createClassroom, joinClassroom, getJoinedStudents } = require("../controllers/classroom.controller");
 const { authMiddleware } = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.get("/joined", authMiddleware, getJoinedClassrooms);
 router.get("/created", authMiddleware, getCreatedClassrooms);
+router.get("/students/:classroom_id", authMiddleware, getJoinedStudents);
 router.get("/:id", authMiddleware, getClassroomById);
 router.post("/create", authMiddleware, createClassroom);
 router.post("/join", authMiddleware, joinClassroom);
