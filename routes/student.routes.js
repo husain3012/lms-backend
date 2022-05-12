becfd4c3aebe9c2151b9cd1faae316fe2b7c848f
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-05-12 18:52:47 +0530

    faltu ka code hatadiya

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const cors = require("cors");
-router.use(cors());
 const { authMiddleware } = require("../middleware/auth.middleware");
 const { getUsers, signup, login, addFakeUsers, updateDetails } = require("../controllers/student.controller");
 
 router.get("/get_all_users", getUsers);
 router.post("/signup", signup);
-router.post("/login", cors(), login);
+router.post("/login", login);
 router.post("/add_fake_users", addFakeUsers);
 router.post("/update_details", authMiddleware, updateDetails);
 
